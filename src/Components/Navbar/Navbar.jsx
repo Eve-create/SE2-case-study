e99fa377b7ad84cf0aac454bc438f67e84e4946a
Eve-create <tisadoeverlyn42@gmail.com>
2025-08-16 18:13:45 +0800

    Initial commit - React app

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react' 
+import './Navbar.css'
+import {LuSearch} from "react-icons/lu";
+import {LuUserRound} from "react-icons/lu";
+import {LuShoppingCart} from "react-icons/lu";
+
+
+const Navbar = () => {
+  return (
+    <div className='nav-bar'>
+        <p>SHOPPER</p>
+        <ul className="nav-menu">
+            <li>About</li>
+            <li>Online Fundraising</li>
+            <li>Mobile Photography</li>
+        </ul>
+        <div> 
+          <ul className="searchProfileCart">
+            <li><LuSearch size={30}/></li>
+            <li><LuUserRound size={30}/></li>
+            <li><LuShoppingCart size={30}/></li>
+          </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
